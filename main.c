e802866675d74e21a494e6f3f1b3a649c5a8ca97
Justin Lubin <jlubi333@gmail.com>
2015-04-23 20:32:44 -0400

    Remove variables.

--- main.c
+++ main.c
@@ -3,26 +3,6 @@
 #include <time.h>
 #include "julina.h"
 
-#define VARIABLE_START 'A'
-#define VARIABLE_COUNT 26
-static Matrix *variables[VARIABLE_COUNT];
-
-Matrix *get_variable(int c) {
-    if (c < 0 || c >= VARIABLE_COUNT) {
-        return NULL;
-    }
-    return variables[c - VARIABLE_START];
-}
-
-// Returns -1 on failure, 0 on success.
-int set_variable(int c, Matrix *a) {
-    if (c < 0 || c >= VARIABLE_COUNT) {
-        return -1;
-    }
-    variables[c - VARIABLE_START] = a;
-    return 0;
-}
-
 /*
  * Format:
  * 3 2 : 1 2 3 4 5 6
